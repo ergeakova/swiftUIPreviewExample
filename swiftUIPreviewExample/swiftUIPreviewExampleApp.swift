4e0f509a5e3e331086f4a7cda3f5b7b9b89c6bd3
ergeakova <ergeak@gmail.com>
2022-07-19 21:46:17 +0300

    Initial Commit

---
+++ swiftUIPreviewExample/swiftUIPreviewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIPreviewExampleApp.swift
+//  swiftUIPreviewExample
+//
+//  Created by Erge Gevher Akova on 19.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIPreviewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
